f4de3871793c6da3ef1aed49e9b9a31965286be7
Labontese <labontese@gmail.com>
2025-12-13 08:04:30 +0100

    chore: extract chrome-extension to separate repository

--- packages/chrome-extension/src/utils/axe-inject.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Axe-core injection script
- * This file bundles axe-core and exposes it to the page
- */
-import axe from 'axe-core';
-
-// Expose axe globally for the scan
-(window as any).axe = axe;
